b82d2fabf3831db5d4b7ce5d32e321aed219424e
JDRV <jodaniel09@gmail.com>
2022-12-21 20:19:29 -0600

    Add urls.py and views.py
    Create the urls.py containing the urlspatterns. Link views.py with project urls.py

--- social_net/core/views.py
+++ social_net/core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('<h1>Welcome to Social Network</h1>')
