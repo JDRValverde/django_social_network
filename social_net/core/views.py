3d1d84253285e3f11afdad7120915b633fcd0968
JDRV <jodaniel09@gmail.com>
2022-12-29 12:04:24 -0600

    add settings section

--- social_net/core/views.py
+++ social_net/core/views.py
@@ -13,6 +13,12 @@ def index(request):
     return render(request, 'core/index.html')
 
 
+@login_required(login_url='signin')
+def settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    return render(request, 'core/setting.html')
+
+
 def signup(request):
 
     if request.method == "POST":
@@ -35,12 +41,16 @@ def signup(request):
 
                 # log user in and direct him to settings page
 
+                user_login = auth.authenticate(
+                    username=username, password=password)
+                auth.login(request, user_login)
+
                 # create a profile object for the new user
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(
                     user=user_model, id_user=user_model.id)
                 new_profile.save()
-                return redirect('signup')
+                return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
