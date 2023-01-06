7ea7435c9cceca90c9e6af21311f5c024160b6ac
JDRV <jodaniel09@gmail.com>
2023-01-06 13:27:39 -0600

    Edit Post model field
    Edited the max_length of Post's post_id

--- social_net/core/models.py
+++ social_net/core/models.py
@@ -33,7 +33,7 @@ class Post(models.Model):
 
 
 class LikePost(models.Model):
-    post_id = models.CharField(max_length=100)
+    post_id = models.CharField(max_length=500)
     username = models.CharField(max_length=100)
 
     def __str__(self):
