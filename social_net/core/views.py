9b7a27e6fe92a2f61b602838fa07ce9885ebe7b7
JDRV <jodaniel09@gmail.com>
2023-01-05 18:53:06 -0600

    Add Post Feed functionalty and LikePost model

--- social_net/core/views.py
+++ social_net/core/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from .models import Profile
+from .models import Profile, Post
 
 # Create your views here.
 
@@ -12,12 +12,30 @@ from .models import Profile
 def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
-    return render(request, 'core/index.html', {'user_profile': user_profile})
+
+    posts = Post.objects.all()
+    return render(request, 'core/index.html', {'user_profile': user_profile, 'posts': posts})
+
+
+@login_required(login_url='signin')
+def like_post(request):
+    pass
 
 
 @login_required(login_url='signin')
 def upload(request):
-    return HttpResponse('<p>hi</p>')
+
+    if request.method == "POST":
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/')
 
 
 @login_required(login_url='signin')
